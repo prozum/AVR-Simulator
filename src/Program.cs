636242955f48b6b8422687b451a83f1503737fd2
Kasper Steensig <kasper.steensig@gmail.com>
2018-05-05 23:34:05 +0200

    moved to dotnet core

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AVR_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Interpreter = new Atmega328Interpreter();
+
+            Interpreter.Load(IntelHEX.Parse("Blink.hex"));
+			//Interpreter.PORTB.PB5.ValueChanged += new EventHandler<AVRInterpreter.GPIOPinValueChangedEventArgs>(PB5_ValueChanged);
+			Interpreter.Execute();
+        }
+    }
+}
